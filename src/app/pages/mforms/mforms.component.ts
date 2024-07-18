3e4414625d68c026b4c909f7b3c07752e81bcd06
Jason Escobar <jasonescobar20@outlook.com>
2024-07-18 12:48:26 -0600

    first commit

---
+++ src/app/pages/mforms/mforms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mforms',
+  standalone: true,
+  imports: [],
+  templateUrl: './mforms.component.html',
+  styleUrl: './mforms.component.scss'
+})
+export class MformsComponent {
+  constructor() {}
+}
